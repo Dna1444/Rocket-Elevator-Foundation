850c31581f99d5a843926ac11e64145b1833e587
Dna1444 <d_duquette@hotmail.com>
2021-04-14 14:40:29 -0400

    comment out elevator media

--- lib/ElevatorMedia.rb
+++ lib/ElevatorMedia.rb
@@ -1,36 +1,36 @@
 
-module ElevatorMedia
-    require 'uri'
-    require 'net/http'
-    require 'openssl'
-    require 'json'
-    require 'rails_helper'
+# module ElevatorMedia
+#     require 'uri'
+#     require 'net/http'
+#     require 'openssl'
+#     require 'json'
+#     require 'rails_helper'
 
-    class Streamer
-        def self.getContent(input)
+#     class Streamer
+#         def self.getContent(input)
             
 
-            url = URI("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=#{input}&day=today")
+#             url = URI("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=#{input}&day=today")
 
-            http = Net::HTTP.new(url.host, url.port)
-            http.use_ssl = true
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#             http = Net::HTTP.new(url.host, url.port)
+#             http.use_ssl = true
+#             http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-            request = Net::HTTP::Post.new(url)
-            request["x-rapidapi-key"] = ENV['aztroApi']
-            request["x-rapidapi-host"] = 'sameer-kumar-aztro-v1.p.rapidapi.com'
+#             request = Net::HTTP::Post.new(url)
+#             request["x-rapidapi-key"] = ENV['aztroApi']
+#             request["x-rapidapi-host"] = 'sameer-kumar-aztro-v1.p.rapidapi.com'
 
 
-            response = http.request(request)
-            puts JSON.parse(response.read_body)['description']
-            pp response
-            # response.read_body
-            description = JSON.parse(response.read_body)['description']
+#             response = http.request(request)
+#             puts JSON.parse(response.read_body)['description']
+#             pp response
+#             # response.read_body
+#             description = JSON.parse(response.read_body)['description']
 
             
-            "<div class = 'text-center'><h3>your aztro</h3><p id = 'aztro'>#{description}</p></div>"
+#             "<div class = 'text-center'><h3>your aztro</h3><p id = 'aztro'>#{description}</p></div>"
             
-        end
+#         end
         
-    end
-end
+#     end
+# end
